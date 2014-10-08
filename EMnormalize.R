e652cef9c60cef1c0ac94a04e53dbd54d4add500
Frederic Commo <fredcommo@Mac-Pro-de-Frederic.local>
2014-10-08 14:59:10 +0200

    Example added in README.md

--- EMnormalize.R
+++ EMnormalize.R
@@ -128,31 +128,4 @@ EMnormalize <- function(x, cut=c(.05, .95), G=3:6, B=100, peakThresh=0.95, ksmoo
 return(list(original=x, corrected=x-correct, means=m, sd=s, props=p, correction=correct))
 }
 
-####################################
-# Example
-op <- par(no.readonly=TRUE)
-set.seed(112335)
-N <- 4
-p.init <- runif(N-1, .1, .3)
-p.init <- c(p.init, 1- sum(p.init))
-x <- lapply(1:N, function(ii) rnorm(round(1e4*p.init[ii]), ii, .5) )
-x <- do.call(c, x)
-
-# Original EM
-model <- Mclust(x)
-m <- model$parameters$mean
-s <- model$parameters$variance$sigmasq
-p <- model$parameters$pro
-D <- computeDensities(length(x), m, s, p)
-bestPeak <- chooseBestPeak(D$peaks, m, peakThresh=.95)
-
-par(mfrow=c(3, 1))
-plotEMmodel(x, m, s, p, bestPeak, xlim=range(0, 6), ylim=range(0, .6))
-
-# Resampling method
-xnorm <- EMnormalize(x, xlim=range(0, 6), ylim=range(0, .6))
-
-# True groups
-plotEMmodel(x, 1:4, rep(0.5, 4), p.init, which.max(p.init), xlim=range(0, 6), ylim=range(0, .6))
-par(op)
 
